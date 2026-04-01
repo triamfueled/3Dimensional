759fc6b7efe655413854e662535b5686bb988d13
triamfueled <tr.iamfueled@gmail.com>
2026-04-01 17:45:04 -0400

    Rebuild with correct paths

--- working/vite.config.ts
+++ working/vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/3dimensional/',
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/3dimensional/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/3Dimensional/',
+})
